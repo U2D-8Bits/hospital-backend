7b05a71336a951abdd0073413097423090fb81c6
U2D-8Bits <u2d.8bits@gmail.com>
2024-09-22 18:23:09 -0500

    Instalación de Mongoose

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const express = require('express');
 // Crear un servidor de express
 const app = express();
 
+// Rutas
+app.get( '/', (req, res)=>{
+    
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo'
+    });
+
+
+});
+
 app.listen( 3001, () => {
     console.log('Servidor en puerto 3001');
 });
