ea291a32385568670cd9c97ca916ec105fbb592e
U2D-8Bits <u2d.8bits@gmail.com>
2024-09-22 19:04:29 -0500

    Instalación y Configuración del CORS

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express');
 require('dotenv').config();
 
+const cors = require('cors');
+const express = require('express');
+
 const { dbConnection } = require('./database/config');
+
 // Crear un servidor de express
 const app = express();
 
+// Configuración de CORS
+app.use(cors());
+
 // Base de datos
 dbConnection();
+
 // Rutas
 app.get( '/', (req, res)=>{
     
