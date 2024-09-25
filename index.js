df56c03a7f021b923d808897607c8d687acdd318
U2D-8Bits <u2d.8bits@gmail.com>
2024-09-25 15:05:17 -0500

    Incorporación de funcionalidad de busqueda general y por colección a través de rutas

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use( '/api/users', require('./routes/user-routes') );
 app.use( '/api/auth', require('./routes/auth-routes') );
 app.use( '/api/hospitals', require('./routes/hospitals-routes') );
 app.use( '/api/doctors', require('./routes/doctor-routes') );
+app.use( '/api/search', require('./routes/search-routes'));
 
 app.listen( process.env.PORT, () => {
     console.log(`Servidor en puerto ${process.env.PORT}`);
