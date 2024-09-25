907fe892b6b472a76b16d4f42ed85e4d9fda46f6
U2D-8Bits <u2d.8bits@gmail.com>
2024-09-24 22:16:59 -0500

    Creación de Controlador y rutas para hospitals

---
+++ controllers/hospitals-controller.js
@@ -0,0 +1,45 @@
+//? -------------- Imports --------------
+
+const { response } = require("express");
+const Hospital = require("../models/hospital-model");
+
+
+//? -----------------------------------------------------
+//? Controller to get all hospitals
+//? -----------------------------------------------------
+
+const getHospitals = async (req, res = response) => {
+
+}
+
+
+//? -----------------------------------------------------
+//? Controller to create a new hospital
+//? -----------------------------------------------------
+
+const createHospital = async (req, res = response) => {
+
+}
+
+//? -----------------------------------------------------
+//? Controller to update a hospital
+//? -----------------------------------------------------
+
+const updateHospital = async (req, res = response) => {
+
+}
+
+//? -----------------------------------------------------
+//? Controller to delete a hospital
+//? -----------------------------------------------------
+
+const deleteHospital = async (req, res = response) => {
+
+}
+
+module.exports = {
+    getHospitals,
+    createHospital,
+    updateHospital,
+    deleteHospital
+}
