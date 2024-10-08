01fe416e999f2c0c6cf1dfe9a23932caabf087cf
U2D-8Bits <u2d.8bits@gmail.com>
2024-10-08 16:57:13 -0500

    Refactorización del hospital routes y controller para incluir el metodo de obtener todos los hospitales

--- routes/hospitals-routes.js
+++ routes/hospitals-routes.js
@@ -9,16 +9,22 @@ const { check } = require('express-validator');
 const { validateJWT } = require('../middlewares/validate-jwt');
 const { validateFields } = require('../middlewares/validate-fields');
 
-const { getHospitals, createHospital, updateHospital, deleteHospital, getHospitalById } = require('../controllers/hospitals-controller');
+const { getHospitals, createHospital, updateHospital, deleteHospital, getHospitalById, getAllHospitals } = require('../controllers/hospitals-controller');
 
 const router = Router();
 
 //? ---------------------------------------------------
-//? --------------- Gets Hospitals Route --------------
+//? --------------- Gets Hospitals With Pagination Route --------------
 //? ---------------------------------------------------
 
 router.get('/', getHospitals);
 
+
+//? ---------------------------------------------------
+//? --------------- Get All Hospital --------------
+//? ---------------------------------------------------
+
+router.get('/all', [validateJWT], getAllHospitals )
 
 //? ---------------------------------------------------
 //? --------------- Get Hospital by ID Route ----------
